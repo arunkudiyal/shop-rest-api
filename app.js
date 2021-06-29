227d754093fced3efa81e390320f068c8aec1d22
Arun Kudiyal <arunkudiyal@icloud.com>
2021-06-29 12:17:21 +0530

    Handling Errors in app.js

--- app.js
+++ app.js
@@ -15,4 +15,20 @@ app.use(morgan('dev'))
 app.use('/products', productsRoute)
 // app.use('/orders', ordersRoute)
 
+// HANDLE MY ERRORS -> a URL which is not a part of our API
+app.use((req, res, next) => {
+    const error = new Error('Not Found')
+    error.status = 400
+    next(error)
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
+
 module.exports = app
