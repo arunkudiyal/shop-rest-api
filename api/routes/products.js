122e8c86335460c5ab9f496501d9567ed08de863
Arun Kudiyal <arunkudiyal@icloud.com>
2021-06-29 11:52:11 +0530

    productsRoute, nodemon & morgan

--- api/routes/products.js
+++ api/routes/products.js
@@ -14,4 +14,39 @@ router.post('/', (req, res, next) => {
     })
 })
 
+// GET - /products/productID 
+// PATCH - /products/productID -> Edit the product
+// DETEE - /products/productId -> Delete the product
+
+// /products/{something}
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId
+    if(id === 'special') {
+        res.status(200).json({
+            message: 'You have a Special ID',
+            id: id
+        })
+    } else {
+        res.status(200).json({
+            message: 'You have an Ordinary ID',
+            id: id
+        })
+    }
+})
+
+// Patch/Edit the products
+router.patch('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling PATCH request to /products/productId',
+        id: req.params.productId
+    })
+})
+
+router.delete('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling DELETE request to /products/productId',
+        id: req.params.productId
+    })
+})
+
 module.exports = router
--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 // Step 2 - Configure the Server
 
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
+// Manaage my routes
 const productsRoute = require('./api/routes/products')
+// const ordersRoute = require('./api/routes/orders')
 
+// Log the route in the console before it reaches to the app.use()
+app.use(morgan('dev'))
+
+// USING THE ROUTES
 app.use('/products', productsRoute)
+// app.use('/orders', ordersRoute)
 
 module.exports = app
